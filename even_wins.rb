28fd59842bf2470b62b90a5c09fd669ccbe5de61
michaelkasenbacher <michael.kasenbacher@gmail.com>
2022-06-15 12:28:14 +0000

    enter

--- even_wins.rb
+++ even_wins.rb
@@ -1,12 +1,12 @@
 # A game of chance to be played against the computer. Whoever wins 5 times is the overall winner. 
 
 # Questions:
-# 1) if I comment "+= 1" in who_wins? and uncomment keep_score I get strange results: 
+# 1) can this be done without the use of instance variables? The reason why I used instance variables was
+# that I got this error message otherwise: "undefined local variable for main:Object (NameError)" 
+
+# 2) if I comment "+= 1" in who_wins? and uncomment keep_score I get strange results: 
 # instead of puts "the number is ... " and "x wins" once, I get puts 3 times but only the last time
 # registers in score. 
-
-# 2) can this be done without the use of instance variables? The reason why I used instance variables was
-# that I got this error message otherwise: "undefined local variable for main:Object (NameError)" 
 
 def get_numbers
   puts "Enter a number (1-100) to play."
